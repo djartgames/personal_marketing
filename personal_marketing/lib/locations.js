c7f370313557a9c827eb1454ba40cd4f05b7388e
djartgames <djart.games@gmail.com>
2026-05-23 00:28:48 -0300

    Add image for locations (#7)

--- personal_marketing/lib/locations.js
+++ personal_marketing/lib/locations.js
@@ -1,9 +1,11 @@
 import { Location } from 'edwin';
+import homeImage from '../assets/images/locations/home.png';
 
 const home = new Location({
   id: 'home',
   name: 'Home',
   description: 'The place where Anastacia lives and works. It feels familiar and safe.',
+  image: homeImage,
   paths: {},
 });
 
