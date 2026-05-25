843667310176fd1e337eb0d9c0357cd518069d0b
djartgames <djart.games@gmail.com>
2026-05-25 14:27:34 -0300

    Refactor locations

--- personal_marketing/lib/locations/index.js
+++ personal_marketing/lib/locations/index.js
@@ -2,5 +2,9 @@ export { bathroom } from './bathroom.js';
 export { bedroom } from './bedroom.js';
 export { coffeeShop } from './coffee_shop.js';
 export { grocery } from './grocery.js';
-export { livingRoom } from './living_room.js';
+export { LivingRoom } from './living_room.js';
 export { townSquare } from './town_square.js';
+
+import { LivingRoom } from './living_room.js';
+const livingRoom = new LivingRoom();
+export { livingRoom };
--- personal_marketing/lib/locations/living_room.js
+++ personal_marketing/lib/locations/living_room.js
@@ -1,16 +1,20 @@
 import { Location } from 'edwin';
 import homeImage from '../../assets/images/locations/home.png';
 
-const livingRoom = new Location({
-  id: 'living_room',
-  name: 'Living Room',
-  description: 'The heart of Anastacia\'s home. It feels familiar and safe.',
-  image: homeImage,
-  paths: {
-    bedroom: { target: 'bedroom' },
-    bathroom: { target: 'bathroom' },
-    town_square: { target: 'town_square', label: 'Outside' },
-  },
-});
+class LivingRoom extends Location {
+  constructor() {
+    super({
+      id: 'living_room',
+      name: 'Living Room',
+      description: 'The heart of Anastacia\'s home. It feels familiar and safe.',
+      image: homeImage,
+      paths: {
+        bedroom: { target: 'bedroom' },
+        bathroom: { target: 'bathroom' },
+        town_square: { target: 'town_square', label: 'Outside' },
+      },
+    });
+  }
+}
 
-export { livingRoom };
+export { LivingRoom };
